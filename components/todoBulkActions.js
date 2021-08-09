211c59460d619851b4285507be2e6e510405e3a6
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-08-09 03:11:27 -0700

    [Update] Styled the list footer containing bulk actions/filtering

--- components/todoBulkActions.js
+++ components/todoBulkActions.js
@@ -1,4 +1,9 @@
+import styles from '../styles/BulkActions.module.scss'
+import { useState } from 'react'
+
 export default function TodoBulkActions({ numLeft, callback, onUpdate }) {
+  const [filter, setFilter] = useState('all')
+
   const deleteCompletedTodos = () => {
     fetch(`/api/todos/${session.user.email}`, {
       method: 'DELETE',
@@ -9,13 +14,19 @@ export default function TodoBulkActions({ numLeft, callback, onUpdate }) {
     })
     onUpdate()
   }
+
+  const setActive = (query) => {
+    setFilter(query)
+    callback(query)
+  }
+
   return (
-    <>
+    <article className={styles.group}>
       <p>{numLeft} todo(s) left</p>
-      <button onClick={() => { callback('all') }}>All</button>
-      <button onClick={() => { callback('pending') }}>Pending</button>
-      <button onClick={() => { callback('complete') }}>Complete</button>
-      <button onClick={() => { deleteCompletedTodos() }} >Delete completed</button>
-    </>
+      <button data-active={filter === 'all'} className={styles.btn} onClick={() => { setActive('all') }}>All</button>
+      <button data-active={filter === 'pending'} className={styles.btn} onClick={() => { setActive('pending') }}>Pending</button>
+      <button data-active={filter === 'complete'} className={styles.btn} onClick={() => { setActive('complete') }}>Complete</button>
+      <button className={styles.btn} onClick={() => { deleteCompletedTodos() }} >Delete completed</button>
+    </article>
 )
 }
